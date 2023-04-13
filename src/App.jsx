d642a21a726e9036e14463ece70d0fba6f6b43a4
Jabid Sierra <jabidjunior@gmail.com>
2023-04-13 04:45:18 -0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import React from 'react'
 import './App.css'
+import axios from 'axios';
+import Weather from './components/weather.jsx'
+const API_endpoint = 'https://api.openweathermap.org/data/2.5/weather?';
+const API_key = 'e4f956c131b0659993c97ae45bfc73b6';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+const [weatherData, setWeatherData] = useState ({})
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      
+      let finalAPIEndPoint = `${API_endpoint}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_key}`;
 
-  return (
+    axios
+    .get(finalAPIEndPoint)
+    .then((resp)=> {
+      setWeatherData(resp.data)
+      console.log(resp.data)
+    }) 
+    })
+    
+  }, [])
+
+  return (   
     <div className="App">
-      <div className='Card-Weather'>
-      <h1>Esta es la tarjeta del clima jajajajajjajcbasj sdb shjdb s</h1>
-      </div>
+      <Weather locationData = {weatherData}/>
     </div>
   )
 }
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const weather = () => {
-    return (
-        <div>
-            
+
+const Weather = ({locationData}) => {
+    const [celcius, setCelsius] = useState(true)
+    console.log(locationData.name)
+
+  return (
+    <div className="card">
+      <div className="icon"><img src={`/${locationData.weather?.[0].icon}.svg`} alt="" className="icon"/></div>
+      <div className="info">
+        <div className="location">
+          {locationData.name}, {locationData.sys?.country}
         </div>
-    );
+        <div className="temperature">
+        { celcius ? `${(locationData.main?.temp - 273.5).toFixed(0)}°` : `${((locationData.main?.temp - 273.5) * 9 / 5 + 32).toFixed(0)}°`}
+        <span className="span">{ celcius ? "C" : "F" }</span>
+        </div>
+        <button className="button" onClick={ () => setCelsius(!celcius)}>{celcius ? "Cambiar a F°" : "Cambiar a C°"}</button>
+      </div>
+    </div>
+  );
 };
 
-export default weather;
+export default Weather;
