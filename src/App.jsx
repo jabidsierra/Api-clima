71211b22e585aff8835cc9238bc5713c4959efc5
Jabid Sierra <jabidjunior@gmail.com>
2023-04-13 00:48:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='Card-Weather'>
+      <h1>Esta es la tarjeta del clima jajajajajjajcbasj sdb shjdb s</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
